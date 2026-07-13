72c67953b5f8821b065bec99ba891f0ecaad337e
Laura Najera <lauranajeram@gmail.com>
2026-07-12 19:59:55 -0600

    Update CORS for production

--- server.js
+++ server.js
@@ -37,9 +37,7 @@ app.use(helmet({
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://localhost:5175',
-  'https://b-com.bz',
-  'https://www.b-com.bz',
+  'https://bcombelize.vercel.app/',
   process.env.FRONTEND_URL
 ].filter(Boolean);
 
